8b8684a5c4eb9a04e361cc32866382e40b2ce9f1
Poulami Mandol <PoulamiM@kol.ntpl>
2021-09-29 18:09:34 +0530

    Zoom_Crop_Rotate_Clear

---
+++ ImageWork/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace ImageWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.Browser = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(852, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Zoom In";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(852, 192);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Zoom Out";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(852, 357);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Crop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(852, 443);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Clear";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Browser
+            // 
+            this.Browser.Location = new System.Drawing.Point(35, 49);
+            this.Browser.Name = "Browser";
+            this.Browser.Size = new System.Drawing.Size(75, 23);
+            this.Browser.TabIndex = 4;
+            this.Browser.Text = "Browser";
+            this.Browser.UseVisualStyleBackColor = true;
+            this.Browser.Click += new System.EventHandler(this.Browser_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(149, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(671, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(35, 98);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(75, 368);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(149, 98);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(671, 368);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(852, 276);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Rotate";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(997, 497);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Browser);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button Browser;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ ImageWork/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Drawing2D;
+using System.Reflection;
+using System.Drawing.Imaging;
+
+namespace ImageWork
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        String abd;
+
+       
+
+        //crop from picturebox
+        int cropX;
+        int cropY;
+        int cropWidth;
+        int cropHeight;
+        Pen cropPen = new Pen(Color.Yellow, 2);
+        Bitmap cropBitmap;
+        Bitmap bm_dest;
+        Bitmap bm_source;
+        int i = 1;
+        FolderBrowserDialog s_fold = new FolderBrowserDialog();
+        FolderBrowserDialog d_fold = new FolderBrowserDialog();
+
+        string[] files;
+        int pheight;
+        int pwidth;
+
+
+        private void Browser_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                listBox1.Items.Clear();
+                string[] folders = Directory.GetFiles(fbd.SelectedPath);
+                textBox1.Text = fbd.SelectedPath;
+                abd = fbd.SelectedPath;
+
+                foreach (string folder in folders)
+                {
+                    listBox1.Items.Add(Path.GetFileName(folder));
+                }
+            }
+
+            //crop from picturebox
+
+            pictureBox1.Width = pwidth;
+            pictureBox1.Height = pheight;
+            Type pboxType = pictureBox1.GetType();
+            PropertyInfo irProperty = pboxType.GetProperty("ImageRectangle", BindingFlags.GetProperty | BindingFlags.NonPublic | BindingFlags.Instance);
+            Rectangle rectangle = (Rectangle)irProperty.GetValue(pictureBox1, null);
+
+            pictureBox1.Height = rectangle.Height;
+            pictureBox1.Width = rectangle.Width; 
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string path = abd + "\\" + listBox1.SelectedItem.ToString();
+            pictureBox1.Image = new Bitmap(path.ToString());
+            System.Drawing.Image img = System.Drawing.Image.FromFile(@"E:\Picture\download.jfif");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        { }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+                pictureBox1.Dispose();
+
+            //crop from picturebox
+
+            pictureBox1.Size = new Size(pictureBox1.Width, Height - (Height * 5 / 100));
+            pheight = pictureBox1.Height;
+            pwidth = pictureBox1.Width;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+              //Rotate
+            Bitmap b1 = new Bitmap(@"E:\Picture\download.jfif");
+
+            pictureBox1.Image = rotateImage(b1, 60f);
+         }
+
+
+        //Rotate
+
+        private Bitmap rotateImage(Bitmap b, float angle)
+        {
+
+            int maxside = (int)(Math.Sqrt(b.Width * b.Width + b.Height * b.Height));
+
+            //create a new empty bitmap to hold rotated image
+
+            Bitmap returnBitmap = new Bitmap(maxside, maxside);
+
+            //make a graphics object from the empty bitmap
+
+            Graphics g = Graphics.FromImage(returnBitmap);
+
+            //move rotation point to center of image
+
+            g.TranslateTransform((float)b.Width / 2, (float)b.Height / 2);
+
+            //rotate
+
+            g.RotateTransform(angle);
+
+            //move image back
+
+            g.TranslateTransform(-(float)b.Width / 2, -(float)b.Height / 2);
+
+            //draw passed in image onto graphics object
+
+            g.DrawImage(b, new Point(0, 0));
+
+            return returnBitmap;
+        }
+
+      private void button3_Click(object sender, EventArgs e)
+        {
+            //crop from picturebox
+
+            Cursor = Cursors.Default;
+            if (cropWidth < 1)
+            {
+                return;
+            }
+            Rectangle rect = new Rectangle(cropX, cropY, cropWidth, cropHeight);  
+            Bitmap OriginalImage = new Bitmap(pictureBox1.Image, pictureBox1.Width, pictureBox1.Height);
+            Bitmap img = new Bitmap(cropWidth, cropHeight);
+            Graphics g = Graphics.FromImage(img);
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            g.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+            g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+            g.DrawImage(OriginalImage, 0, 0, rect, GraphicsUnit.Pixel);
+            pictureBox1.Image = img;
+            pictureBox1.Width = img.Width;
+            pictureBox1.Height = img.Height;
+            //pictureBox1Location();
+            //btnCrop.Enabled = false;         
+        }
+
+
+        // Zoom
+          private Size Multiplier;
+
+        public void ZoomIn()
+        {
+            Multiplier = new Size(2,2);
+
+            Image MyImage = pictureBox1.Image;
+
+            Bitmap MyBitMap = new Bitmap(MyImage, Convert.ToInt32(MyImage.Width * Multiplier.Width),
+                Convert.ToInt32(MyImage.Height * Multiplier.Height));
+
+            Graphics Graphic = Graphics.FromImage(MyBitMap);
+
+            Graphic.InterpolationMode = InterpolationMode.High ;
+
+            pictureBox1.Image = MyBitMap;
+
+        }
+
+        public void ZoomOut()
+        {
+            Multiplier = new Size(2, 2);
+
+            Image MyImage = pictureBox1.Image;
+
+            Bitmap MyBitMap = new Bitmap(MyImage, Convert.ToInt32(MyImage.Width / Multiplier.Width),
+                Convert.ToInt32(MyImage.Height / Multiplier.Height));
+
+            Graphics Graphic = Graphics.FromImage(MyBitMap);
+
+            Graphic.InterpolationMode = InterpolationMode.High ;
+
+            pictureBox1.Image = MyBitMap;
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+          ZoomIn();
+        }
+
+       
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {}
+
+       
+
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            //crop from picturebox
+
+              if (e.Button == MouseButtons.Left)
+            {
+                cropX = e.X;
+                cropY = e.Y;
+                cropPen.DashStyle = DashStyle.Solid;
+                Cursor = Cursors.Cross;
+            }
+            pictureBox1.Refresh();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //crop from picturebox
+
+            if (e.Button == MouseButtons.Left)
+            {
+                if (pictureBox1.Image == null)
+                    return;
+
+                pictureBox1.Refresh();
+                cropWidth = e.X - cropX;
+                cropHeight = e.Y - cropY;
+                Graphics g = pictureBox1.CreateGraphics();
+                g.DrawRectangle(cropPen, cropX, cropY, cropWidth, cropHeight);
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            //crop from picturebox
+
+            try
+            {
+                Cursor = Cursors.Default;
+                try
+                {
+
+                    if (cropWidth < 1)
+                        return;
+
+                    Rectangle rect = new Rectangle(cropX, cropY, cropWidth, cropHeight);
+                    Bitmap bit = new Bitmap(pictureBox1.Image, pictureBox1.Width, pictureBox1.Height);
+
+                    cropBitmap = new Bitmap(cropWidth, cropHeight);
+                    Graphics g = Graphics.FromImage(cropBitmap);
+                    g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                    g.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+                    g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+                    g.DrawImage(bit, 0, 0, rect, GraphicsUnit.Pixel);
+                    cropBitmap.Save("d:\\abc.jpg");
+                }
+                catch { }
+            }
+            catch { }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           ZoomOut();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+        }
+    }
+}
